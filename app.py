bfaea84d13627834009f9f2f176ec6962c6f8968
Ahmed Eid <66841732+A7med3id10@users.noreply.github.com>
2020-10-21 15:20:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from models import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
